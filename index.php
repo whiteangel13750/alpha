6830c2cbc915ba4142872df743da436cb563ec10
Gregory <goutgregoryy@gmail.com>
2020-03-18 10:46:48 +0100

    Première modif

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <title>Mon livre de recettes</title>
+        <meta charset="utf-8">
+         <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+        <header>
+            <nav class="menu">
+                <? require "menu.html" ?>
+            </nav>
+        </header>
