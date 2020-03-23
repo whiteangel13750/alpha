0f7adc6106fb91e6d30fc501440a658af684f3ad
Gregory <goutgregoryy@gmail.com>
2020-03-23 09:18:41 +0100

    modif

--- menu.php
+++ menu.php
@@ -1,4 +1,76 @@
-    <ul>
+    <?
+    
+    $lien = [
+        "recettes/entree.html",
+        "recettes/plat.html",
+        "recettes/dessert.html"]
+
+    $titre = [
+        "Les entrées",
+        "Les plats",
+        "Les desserts"
+    ]
+
+    $titres1 = [
+        "Les entrées",
+        "Les plats",
+        "Les desserts"
+    ]
+
+    $souslien1 =[
+        "recettes/entrees/entree1.html",
+        "recettes/entrees/entree2.html",
+        "recettes/entrees/entree3.html"
+    ]
+
+    $souslien2 =[
+        "recettes/plats/plat1.html",
+        "recettes/plats/plat2.html",
+        "recettes/plats/plat3.html"
+    ]
+
+    $souslien3 =[
+        "recettes/desserts/dessert1.html",
+        "recettes/desserts/dessert2.html",
+        "recettes/desserts/dessert3.html"
+    ]
+
+
+    $souslien = [$souslien1, $souslien2, $souslien3]
+
+    $size = sizeof($souslien)
+
+    $lien = "";
+    $souslien = "";
+    for($i= 0; $i < $size; $i++) {
+        $titre .=$titre[$i]
+        $lien .= $lien[$i];
+        $souslien .= $souslien[$i];
+    ?>
+    
+    <!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <title>Mon livre de recettes</title>
+        <meta charset="utf-8">
+         <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+    
+    <body>
+        <header>
+            <nav class="menu">
+                <ul>
+                <li><a href ="#">Page d'accueil</a></li>
+                <li><a href =<?php $lien?>><?$titress?></a></li>
+                <ul class="sous-menu">
+                <li><a href =<?php $souslien?>></a></li>
+                </ul>
+            <header>
+    </body>   
+    
+    
+    <!-- <ul>
         <li><a href ="#">Page d'accueil</a></li>
         <li><a href ="recettes/entree.html">Les entrées</a>
             <ul class="sous-menu">
@@ -21,4 +93,4 @@
                 <li><a href ="recettes/desserts/dessert3.html">Dessert 3</a></li>
             </ul>
         </li>
-    </ul>
+    </ul> -->
